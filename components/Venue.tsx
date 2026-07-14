b153f18ff82bc11bb0732140b9d729dfaf28be17
Shreeniwas Salian <shreeniwassalian3011@gmail.com>
2026-07-14 19:05:59 +0530

    feat: add clickable Google Maps image widget for venue

--- components/Venue.tsx
+++ components/Venue.tsx
@@ -8,12 +8,22 @@ export default function Venue() {
       <p className="venue-location">Khed Shivapur</p>
       <Divider compact />
       <a
-        className="maps-button"
+        className="map-widget-container"
         href="https://www.google.com/maps/search/?api=1&query=Nakshatra+Palace+Khed+Shivapur"
         target="_blank"
         rel="noreferrer"
+        aria-label="View Nakshatra Palace on Google Maps"
       >
-        View on Google Maps
+        <div className="map-widget-frame">
+          <img
+            src="/google-map-widget.png"
+            alt="Google Maps Location Widget for Nakshatra Palace"
+            className="map-widget-img"
+          />
+          <div className="map-widget-overlay">
+            <span className="map-widget-btn">View on Google Maps</span>
+          </div>
+        </div>
       </a>
     </>
   );
